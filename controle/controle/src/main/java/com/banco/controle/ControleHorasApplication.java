227a32bcf3ae309558619826b5e4fbc593b979ba
LopesJosiel <josielslopes@gmail.com>
2023-06-26 21:01:01 -0300

    criando packages

---
+++ controle/controle/src/main/java/com/banco/controle/ControleHorasApplication.java
@@ -0,0 +1,13 @@
+package com.banco.controle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleHorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleHorasApplication.class, args);
+	}
+
+}
---
+++ controle/controle/src/test/java/com/banco/controle/ControleHorasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.controle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleHorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
